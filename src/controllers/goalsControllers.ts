e94040a5ba8ae38acb329a16f3c3b3dee9e54166
21305479 <21305479@stu.mmu.ac.uk>
2024-05-07 23:59:56 +0100

    getting a goal

--- src/controllers/goalsControllers.ts
+++ src/controllers/goalsControllers.ts
@@ -1,6 +1,6 @@
 async function getGoal(req: Request, res: Response, next: NextFunction) {
   try {
-    const activityId = req.query;
+    const activityId = req.query.id;
     console.log(activityId);
     const activty = await goalsModel.findById(activityId);
     res.json(activty);
