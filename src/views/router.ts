72057e0e1c5194fcdb0b9c1c16c2d1dde3583012
Aliwebs <11ASkhankausar@gmail.com>
2024-05-07 23:28:51 +0100

    activty controler

--- src/views/router.ts
+++ src/views/router.ts
@@ -1,5 +1,10 @@
 import express from 'express'
 const router = express.Router()
+//! User routes
+router.route('/register')
+  .post(userController.register)
 
+router.route('/login')
+  .post(userController.login)
 
 export default router
