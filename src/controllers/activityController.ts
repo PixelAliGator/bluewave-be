6f78dbb5a0afcc7b8bc1dace4282adfeab2291eb
jacob5467 <21390884@stu.mmu.ac.uk>
2024-05-07 23:36:00 +0100

    reading activity controller

---
+++ src/controllers/activityController.ts
@@ -0,0 +1,21 @@
+async function getSwimmingActivity(req: Request, res: Response, next: NextFunction) {
+    try {
+        const activityId = req.query.id;
+        if(activityId === '*'){
+            const activties:swimmingType[] = await swimmingTable.find({});
+            res.json(activties);
+        }
+        else if (activityId) {
+            const id = req.currentUser.id;
+            const activties:swimmingType[] = await swimmingTable.find({});
+            activties.filter((activty) => activty.user_id === id);
+            res.json(activties)
+        } else {
+            const activty = await swimmingTable.findById(activityId)
+            res.json(activty)
+        }
+
+    } catch (e) {
+        next(e)
+    }
+}
